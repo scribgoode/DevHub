2f74d538a5912b3684cebc46428e541dc28f6141
Leland Moy <48741579+AdeptOfficial@users.noreply.github.com>
2025-01-10 13:50:10 -0600

    created websocket
    not sure how to connect websocket with correct url...
    https://channels.readthedocs.io/en/latest/tutorial/part_2.html

--- devhub/asgi.py
+++ devhub/asgi.py
@@ -8,15 +8,25 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devhub.settings')
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devhub.settings')
+import django
+django.setup()
+
+
+from channels.auth import AuthMiddlewareStack
+from text_chat.routing import websocket_urlpatterns as routing
+
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         # Just HTTP for now. (We can add other protocols later.)
+        'websocket': AuthMiddlewareStack(
+            URLRouter(routing)
+        ),
     }
 )
