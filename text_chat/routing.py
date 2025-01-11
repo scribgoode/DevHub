be1fe838a33229c240d1f784176ffff1ad9e81ce
Scrib Goode <adamscribgoode@gmail.com>
2025-01-11 00:00:38 -0600

    started working on fixing web socket connection problem

--- text_chat/routing.py
+++ text_chat/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server/', consumers.ChatConsumer.as_asgi()),
+    #re_path(r'ws/socket-server/', consumers.ChatConsumer.as_asgi()),
+    re_path('ws/text_chat/adamscribner1@gmail.com/', consumers.ChatConsumer.as_asgi()),
 ]
