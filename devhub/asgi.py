73224e3ba8006f1a096461f01c1a7b00d3545012
Leland Moy <48741579+AdeptOfficial@users.noreply.github.com>
2025-01-30 20:14:50 -0600

    clean up merge with development and video_chat

--- devhub/asgi.py
+++ devhub/asgi.py
@@ -1,34 +1,25 @@
-"""
-ASGI config for devhub project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devhub.settings')
 import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rtc_demo.settings')
-django.setup()
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-import django
-django.setup()
-
-
 from channels.auth import AuthMiddlewareStack
-
-django_asgi_app = get_asgi_application()
-from text_chat.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
+# Ensure correct settings module is set once
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devhub.settings")  # Update if rtc_demo is correct
+django.setup()
+
+# Initialize Django ASGI application first
+django_asgi_app = get_asgi_application()
+
+# Import WebSocket routes after Django setup
+from text_chat.routing import websocket_urlpatterns as text_chat_websocket_urlpatterns
+from video_chat.routing import websocket_urlpatterns as video_chat_websocket_urlpatterns
+
+# Combine WebSocket routes
+websocket_urlpatterns = text_chat_websocket_urlpatterns + video_chat_websocket_urlpatterns
+
+# Define ASGI application
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
--- video_chat/apps.py
+++ video_chat/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
 
 
-<<<<<<<< HEAD:text_chat/apps.py
-class TextChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'text_chat'
-========
 class VideoChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'video_chat'
->>>>>>>> video_chat:video_chat/apps.py
