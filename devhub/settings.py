fe3c75446a1811f7f6dcc9f96734ad7370356a6e
Leland Moy <48741579+AdeptOfficial@users.noreply.github.com>
2025-04-25 21:41:28 -0500

    Update settings.py

--- devhub/settings.py
+++ devhub/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'projectpaldb',
-        'USER': 'adept',
+        'USER': 'admin',
         'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
